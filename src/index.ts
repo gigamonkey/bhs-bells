37ba54843b4c7f879230facb6616cc61d73a1ceb
Peter Seibel <peter@gigamonkeys.com>
2024-06-22 12:37:29 -0700

    Install temporal-polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { calendars, Year, Period } from './calendars.ts';
+import { Temporal } from 'temporal-polyfill';
 
+console.log(Temporal.Now.zonedDateTimeISO().toString());
 
 export const isSchoolDay = (date: Date) => true;
 
